ba29281c870f0b005e70c2dfd138e201b95e72c1
superpasses <165167006+superpasses@users.noreply.github.com>
2024-08-02 17:50:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 29726755
+API_HASH     = "0a708211226ff747f38c47a80a5e29c0"
+BOT_TOKEN    = "6826129519:AAERNPqV9nuoW9wuFAVbxvtjG0uXIFfbW8g"
+SESSION      = "BQHFmCMANei5pBPYiph-8gmfM-qNN0-c9PdpNY-MTF3MVyB108ihPs-jnZPhaT7TiP6q3lH5b-lqdSJBiYeNYJ-lJCMc-KZqyvMrY_2iixn5zrHVVR08AGc38qmTAsi_Ja6TxBjxfzuf1h6dPcPVtSX2wZfQAQ6Tm1rLWykijtmR2iBLT4brik8oxid39vIuf-0pYWk7aF1iDcZVLeOq1q4iutObsAkAuHNxnqKtgeGpurzh_int7OIpdA5NI2fngXuF4u0jrnsVZUsr1vHePzUnELcgA4DlYJiZjrV7NgpG5za8StYZSrZbBoBx0RUv9ZxbYCGArqFNNc-8lU29PTi7USvlhgAAAAG99HY2AA"
+DATABASE_URI = "mongodb+srv://deepchutia95:tmpGRbjdJE1WTpG8@cluster0.wrwzavb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002167800454
+ADMIN        = 7481882166
+CHANNEL      = "@MKD_Korean_Drama"
